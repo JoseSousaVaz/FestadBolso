d89061f68eeb424cb4e38298d5bab11b0c1cd89a
Ze <jseguilherme@hotmail.com>
2025-03-14 05:13:19 +0000

    Tinha faltado o game

---
+++ app/src/main/java/com/example/festadbolso/model/Game.java
@@ -0,0 +1,33 @@
+package com.example.festadbolso.model;
+
+public class Game {
+    private String id;
+    private String name;
+    private String description;
+    private String imageUrl;
+
+    // Constructor
+    public Game(String id, String name, String description, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
